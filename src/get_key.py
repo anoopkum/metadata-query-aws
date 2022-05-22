228c2ac09ce197ff3ae67b703376a4034ad56465
Anoop Kumar <43013645+anoopkum@users.noreply.github.com>
2022-05-22 11:28:02 +0100

    Update get_key.py

--- src/get_key.py
+++ src/get_key.py
@@ -1,44 +1,26 @@
-import requests
-import json
-
-metadata_url = 'http://169.254.169.254/latest/'
+from get_metadata import get_metadata
 
 
-def expand_tree(url, arr):
-    output = {}
-    for item in arr:
-        new_url = url + item
-        r = requests.get(new_url)
-        text = r.text
-        if item[-1] == "/":
-            list_of_values = r.text.splitlines()
-            output[item[:-1]] = expand_tree(new_url, list_of_values)
-        elif is_json(text):
-            output[item] = json.loads(text)
-        else:
-            output[item] = text
-    return output
+# https://stackoverflow.com/questions/9807634/find-all-occurrences-of-a-key-in-nested-python-dictionaries-and-lists
+def gen_dict_extract(key, var):
+    if hasattr(var, 'items'):
+        for k, v in var.items():
+            if k == key:
+                yield v
+            if isinstance(v, dict):
+                for result in gen_dict_extract(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in gen_dict_extract(key, d):
+                        yield result
 
 
-def get_metadata():
-    initial = ["meta-data/"]
-    result = expand_tree(metadata_url, initial)
-    return result
-
-
-def get_metadata_json():
+def find_key(key):
     metadata = get_metadata()
-    metadata_json = json.dumps(metadata, indent=4, sort_keys=True)
-    return metadata_json
-
-
-def is_json(myjson):
-    try:
-        json.loads(myjson)
-    except ValueError:
-        return False
-    return True
+    return list(gen_dict_extract(key, metadata))
 
 
 if __name__ == '__main__':
-    print(get_metadata_json())
+    key = input("What key would you like to find?\n")
+    print(find_key(key))
